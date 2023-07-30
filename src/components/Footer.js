5376ac4bbfa20026f7c675d198f6101b769b1796
Mohit <mr6114671@gmail.com>
2023-07-30 15:22:23 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className=' bg-orange-500 py-3 flex flex-col items-center sm:text-sm sm:py-1' >
+      <h3>Made by Mohit Singh Rawat</h3>
+      <h2>contact - mr6114671@gmail.com</h2>
     </div>
   )
 }
